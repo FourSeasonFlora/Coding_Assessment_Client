794f2333f16da331b2791dab1a5539481251cdee
Lauren Acton <fourseasonflora@gmail.com>
2022-03-09 18:24:42 -0500

    Adding project to repo

---
+++ IntegrationSoftwareEngineer_Assessment_LActon/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+int[] numbers = { 1, 2, 3, 4, 5 };
+Dictionary<string, int[]> numberDictionary = new Dictionary<string, int[]>();
+numberDictionary.Add("numbers", numbers);
+
+
+var client = new HttpClient();
+var request = new HttpRequestMessage();
+request.RequestUri = new Uri("http://localhost:7071/api/Endpoint_C");
+request.Content = new StringContent(JsonSerializer.Serialize(numberDictionary));
+
+var response = await client.PostAsync(request.RequestUri,request.Content);
+Console.WriteLine(response.Content.ReadAsStringAsync().Result);
